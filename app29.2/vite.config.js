9d8f0b0be2b6a9b21c7600837e44c6719af250ce
Lera Ulko <leraulko@MacBook-Pro-Lera.local>
2025-06-04 00:04:11 +0300

    added changes

--- app29.2/vite.config.js
+++ app29.2/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app29.2/',
+  base: '/hillel-react-hw29/app29.2/',
 })
